96ce58776f0da6ac144a6203269d20a960a6fdcd
chanhtv235 <chanhtv235@gmail.com>
2024-12-21 19:39:17 +0700

    second

--- src/services/accounServcie.js
+++ src/services/accounServcie.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function checkLogin(loginInfor) {
     try{
-        const response = await axios.get("http://localhost:8080/accounts");
+        const response = await axios.get("https://back-end-c07.onrender.com/accounts");
         const account = response.data.find(ac => ac.username == loginInfor.username && ac.password == loginInfor.password);
         if (account!=null){
             return account;
--- src/services/manufactureService.js
+++ src/services/manufactureService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 export async function getAllManufacture() {
     try{
-        const res = await axios.get("http://localhost:8080/manufactures");
+        const res = await axios.get("https://back-end-c07.onrender.com/manufactures");
         return res.data;
     }catch (e) {
         return []
